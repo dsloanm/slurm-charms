29794f8c4aa9eba7d56291e98f090e7bc4fdf4cb
Dominic Sloan-Murphy <dominic.sloanmurphy@canonical.com>
2024-12-17 16:29:16 +0000

    Remove commented out install method.

--- charms/slurmd/src/utils/gpu.py
+++ charms/slurmd/src/utils/gpu.py
@@ -29,7 +29,6 @@ class GPUInstallError(Exception):
 class GPUDriverDetector:
     """Detects GPU driver and kernel packages appropriate for the current hardware."""
 
-    # # TODO: fix this temp workaround while UbuntuDriver issues are addressed.
     def system_packages(self) -> list:
         """Return a list of GPU drivers and kernel module packages for this node."""
         install_packages = [
@@ -48,66 +47,6 @@ class GPUDriverDetector:
 
         # Failed to find a GPU.
         return []
-
-    # def __init__(self):
-    #     """Initialise detection attributes and interfaces."""
-    #     # Install ubuntu-drivers tool and Python NVML bindings if unavailable.
-    #     # TODO: try->fail->install is a bad pattern. Find an alternative. Install these packages in slurm_ops?
-    #     pkgs = ["ubuntu-drivers-common", "python3-pynvml"]
-    #     try:
-    #         self._detect = import_module("UbuntuDrivers.detect")
-    #     except ModuleNotFoundError:
-    #         try:
-    #             apt.update()
-    #             apt.add_package(pkgs)
-    #         except (apt.PackageNotFoundError, apt.PackageError) as e:
-    #             raise GPUInstallError(f"failed to install {pkgs} reason: {e}")
-    #
-    #         self._detect = import_module("UbuntuDrivers.detect")
-    #
-    #     # ubuntu-drivers requires apt_pkg for package operations
-    #     self._apt_pkg = import_module("apt_pkg")
-    #     self._apt_pkg.init()
-    #
-    # def _system_gpgpu_driver_packages(self) -> dict:
-    #     """Detect the available GPGPU drivers for this node."""
-    #     return self._detect.system_gpgpu_driver_packages()
-    #
-    # def _get_linux_modules_metapackage(self, driver) -> str:
-    #     """Retrieve the modules metapackage for the combination of current kernel and given driver.
-    #
-    #     e.g. linux-modules-nvidia-535-server-aws for driver nvidia-driver-535-server
-    #     """
-    #     return self._detect.get_linux_modules_metapackage(self._apt_pkg.Cache(None), driver)
-    #
-    # def system_packages(self) -> list:
-    #     """Return a list of GPU drivers and kernel module packages for this node."""
-    #     # Detect only GPGPU drivers. Not general purpose graphics drivers.
-    #     packages = self._system_gpgpu_driver_packages()
-    #
-    #     # Gather list of driver and kernel modules to install.
-    #     install_packages = []
-    #     for driver_package in packages.keys():
-    #
-    #         # Ignore drivers that are not recommended
-    #         if packages[driver_package].get("recommended"):
-    #             # Retrieve metapackage for this driver,
-    #             # e.g. nvidia-headless-no-dkms-535-server for nvidia-driver-535-server
-    #             driver_metapackage = packages[driver_package]["metapackage"]
-    #
-    #             # Retrieve modules metapackage for combination of current kernel and recommended driver,
-    #             # e.g. linux-modules-nvidia-535-server-aws
-    #             modules_metapackage = self._get_linux_modules_metapackage(driver_package)
-    #
-    #             # Add to list of packages to install
-    #             install_packages += [driver_metapackage, modules_metapackage]
-    #
-    #     # TODO: do we want to check for nvidia here and add nvidia-fabricmanager-535 libnvidia-nscq-535 in case of nvlink? This is suggested as a manual step at https://documentation.ubuntu.com/server/how-to/graphics/install-nvidia-drivers/#optional-step. If so, how do we get the version number "-535" robustly?
-    #
-    #     # TODO: what if drivers install but do not require a reboot? Should we "modprobe nvidia" manually? Just always reboot regardless?
-    #
-    #     # Filter out any empty results as returning
-    #     return [p for p in install_packages if p]
 
 
 def autoinstall() -> None:
