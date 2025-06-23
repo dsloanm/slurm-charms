9604d256e1f2c9b4f4c53d80d5f062eac376fd80
Dominic Sloan-Murphy <dominic.sloanmurphy@canonical.com>
2025-06-23 12:11:57 +0000

    feat(slurmd): update `slurmd` for `slurmctld` HA

--- charms/slurmd/src/interface_slurmctld.py
+++ charms/slurmd/src/interface_slurmctld.py
@@ -2,7 +2,7 @@
 
 import json
 import logging
-from typing import Union
+from typing import List, Union
 
 from ops import (
     EventBase,
@@ -26,27 +26,27 @@ class SlurmctldAvailableEvent(EventBase):
         handle,
         auth_key,
         nhc_params,
-        slurmctld_host,
+        slurmctld_hosts,
     ):
         super().__init__(handle)
 
         self.auth_key = auth_key
         self.nhc_params = nhc_params
-        self.slurmctld_host = slurmctld_host
+        self.slurmctld_hosts = slurmctld_hosts
 
     def snapshot(self):
         """Snapshot the event data."""
         return {
             "auth_key": self.auth_key,
             "nhc_params": self.nhc_params,
-            "slurmctld_host": self.slurmctld_host,
+            "slurmctld_hosts": self.slurmctld_hosts,
         }
 
     def restore(self, snapshot):
         """Restore the snapshot of the event data."""
         self.auth_key = snapshot.get("auth_key")
         self.nhc_params = snapshot.get("nhc_params")
-        self.slurmctld_host = snapshot.get("slurmctld_host")
+        self.slurmctld_hosts = snapshot.get("slurmctld_hosts")
 
 
 class SlurmctldUnavailableEvent(EventBase):
@@ -135,10 +135,12 @@ class Slurmctld(Object):
 
     def set_node(self) -> None:
         """Set the node on the unit data."""
-        if relation := self._relation:
-            relation.data[self.model.unit]["node"] = json.dumps(self._charm.get_node())
-        else:
-            logger.debug("No relation, cannot set 'node'.")
+        for relation in self._relations:
+            if relation:
+                logger.debug("setting node on relation %s", relation)
+                relation.data[self.model.unit]["node"] = json.dumps(self._charm.get_node())
+            else:
+                logger.debug("No relation, cannot set 'node'.")
 
     def set_partition(self) -> None:
         """Set the slurmd partition on the app relation data.
@@ -147,17 +149,20 @@ class Slurmctld(Object):
         slurmctld application(s) to observe the relation-changed
         event so they can acquire and redistribute the updated slurm config.
         """
-        if relation := self._relation:
-            relation.data[self.model.app]["partition"] = json.dumps(self._charm.get_partition())
-        else:
-            logger.debug("No relation, cannot set 'partition'.")
+        for relation in self._relations:
+            if relation:
+                relation.data[self.model.app]["partition"] = json.dumps(
+                    self._charm.get_partition()
+                )
+            else:
+                logger.debug("No relation, cannot set 'partition'.")
 
     @property
-    def _relation(self) -> Union[Relation, None]:
-        """Return the relation."""
-        return self.model.get_relation(self._relation_name)
+    def _relations(self) -> Union[List[Relation], tuple]:
+        """Return the relation(s)."""
+        return self.model.relations.get(self._relation_name, ())
 
     @property
     def is_joined(self) -> bool:
-        """Return True if relation is joined."""
-        return True if self.model.relations.get(self._relation_name) else False
+        """Return True if relation(s) are joined."""
+        return all(self.model.relations.get(self._relation_name, ()))
