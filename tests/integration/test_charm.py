5c13a5d4d5d69e686ccc07ad7de7bd8a1f9c7b1b
Dominic Sloan-Murphy <dominic.sloanmurphy@canonical.com>
2025-07-16 15:00:37 +0000

    feat(slurmctld): update integration tests for HA

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -44,11 +44,14 @@ def test_deploy(juju: jubilant.Juju, base, sackd, slurmctld, slurmd, slurmdbd, s
         base=base,
         channel=DEFAULT_SLURM_CHARM_CHANNEL if isinstance(sackd, str) else None,
     )
+    # Controller uses a VM with low `SlurmctldTimeout` to facilitate HA tests
     juju.deploy(
         slurmctld,
         SLURMCTLD_APP_NAME,
         base=base,
         channel=DEFAULT_SLURM_CHARM_CHANNEL if isinstance(slurmctld, str) else None,
+        constraints={"virt-type": "virtual-machine"},
+        config={"slurm-conf-parameters": "SlurmctldTimeout=10\n"},
     )
     juju.deploy(
         slurmd,
