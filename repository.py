b65e731b88bb1beb3168454c3ee2c8bd3d6f0d7c
Jason Nucciarone <40342202+NucciTheBoss@users.noreply.github.com>
2026-05-26 23:43:16 +0200

    feat: build Slurm charms with the `uv` plugin instead of the `charm` plugin
    Commit history
    
    ---
    
    * fix: drop `pycryptodome` dependency
    
    This package isn't used by any of the Slurm charms.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * fix: drop `dbus-fast` dependency
    
    This package isn't used by any of our charms currently.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * fix: move `cryptography` and `distro` dependencies to `slurm-ops` workspace
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * feat(all): explicitly declare charm dependencies in workspace pyproject.toml file
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * feat(all): build charms with `uv` plugin instead of the `charm` plugin
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * feat: use `rtoml` instead of `tomllib`
    
    `rtoml` is faster than `tomllib` and has methods for writing out toml files.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * feat: inject local dependencies into pyproject.toml file for `uv` plugin
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    * feat: inject top-level uv.lock file into stage location rather than generate a new lockfile
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>
    
    ---------
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- repository.py
+++ repository.py
@@ -11,13 +11,13 @@ import os
 import shutil
 import subprocess
 import sys
-import tomllib
 from collections.abc import Collection, Mapping, MutableSequence
 from dataclasses import dataclass
 from pathlib import Path
 from threading import Thread
 from typing import Any
 
+import rtoml
 import yaml
 
 ROOT_DIR = Path(__file__).parent.resolve()
@@ -27,7 +27,7 @@ PUBLIC_PKGS_PATH = ROOT_DIR / "pkg"
 PRIVATE_PKGS_PATH = ROOT_DIR / "internal"
 PYPROJECT_FILE = "pyproject.toml"
 CHARMCRAFT_FILE = "charmcraft.yaml"
-LOCK_FILE = "uv.lock"
+LOCK_FILE = ROOT_DIR / "uv.lock"
 LIBS_CHARM_PATH = BUILD_PATH / "libs"
 
 
@@ -164,14 +164,14 @@ class Repository:
         """Load the monorepo information."""
         UV.run_command(["lock", "--quiet"])
         try:
-            with (ROOT_DIR / PYPROJECT_FILE).open(mode="rb") as f:
-                project = tomllib.load(f)
+            with open(ROOT_DIR / PYPROJECT_FILE, mode="r") as fin:
+                project = rtoml.load(fin)
         except OSError:
             raise RepositoryError(f"Failed to read file `{ROOT_DIR / PYPROJECT_FILE}`")
 
         try:
-            with (ROOT_DIR / LOCK_FILE).open(mode="rb") as f:
-                uv_lock = tomllib.load(f)
+            with open(LOCK_FILE) as fin:
+                uv_lock = rtoml.load(fin)
         except OSError:
             raise RepositoryError("Failed to read uv.lock file")
 
@@ -182,22 +182,6 @@ class Repository:
             ]
         except KeyError:
             self.external_libraries = []
-
-        try:
-            binary_packages = project["tool"]["repository"]["binary-packages"]
-
-            resolved_binary_packages = {
-                bin_pkg: package["version"]
-                for bin_pkg in binary_packages
-                for package in uv_lock["package"]
-                if package["name"] == bin_pkg
-            }
-        except KeyError:
-            resolved_binary_packages = {}
-        except StopIteration:
-            raise RepositoryError("Could not find package in the lock file")
-        except OSError:
-            raise RepositoryError(f"Failed to read file `{ROOT_DIR / LOCK_FILE}`")
 
         self.internal_libraries = []
         for charm in CHARMS_PATH.iterdir():
@@ -235,7 +219,6 @@ class Repository:
                     path,
                     libraries=self.libraries,
                     packages=self.packages,
-                    binary_packages=resolved_binary_packages,
                     uv_lock=uv_lock,
                 )
             )
@@ -256,12 +239,11 @@ def load_charm(
     *,
     libraries: Collection[CharmLibrary],
     packages: Collection[Package],
-    binary_packages: Mapping[str, str],
     uv_lock: Mapping[str, Any],
 ) -> Charm | None:
     try:
-        with (charm / PYPROJECT_FILE).open(mode="rb") as f:
-            project = tomllib.load(f)
+        with open(charm / PYPROJECT_FILE, mode="r") as fin:
+            project = rtoml.load(fin)
     except NotADirectoryError:
         logger.info("skipping %s because it is not a charm directory", charm)
         return None
@@ -289,10 +271,6 @@ def load_charm(
                 deps.add(pkg_dep["name"])
                 pending.append(pkg_dep["name"])
 
-    metadata["parts"]["charm"]["charm-binary-python-packages"] = [
-        f"{package}=={version}" for package, version in binary_packages.items() if package in deps
-    ]
-
     libs = []
     try:
         for lib in project["tool"]["repository"]["libraries"]:
@@ -319,8 +297,8 @@ def load_charm(
 
 def load_package(package: Path) -> Package | None:
     try:
-        with (package / PYPROJECT_FILE).open(mode="rb") as f:
-            metadata = tomllib.load(f)
+        with open(package / PYPROJECT_FILE, mode="r") as fin:
+            metadata = rtoml.load(fin)
     except NotADirectoryError:
         logger.info("skipping %s because it is not a package directory", package)
         return None
@@ -373,6 +351,10 @@ def stage_charm(
         except OSError:
             raise RepositoryError(f"Failed to write file `{charm.build_path / CHARMCRAFT_FILE}`")
 
+        # Inject uv lockfile into build directories. The lockfile is used by `charmcraft` to determine
+        # the version of charm dependencies to pull in.
+        shutil.copy(LOCK_FILE, charm.build_path)
+
         # Create a version file and pack it into the charm. This is dynamically generated to ensure
         # that the git revision of the charm is always recorded in this version file.
         git_hash = (
@@ -393,40 +375,38 @@ def stage_charm(
         version_file = Path(charm.build_path / "version")
         version_file.write_text(git_hash)
 
-    for lib in charm.libraries:
-        src = LIBS_CHARM_PATH / "lib" / "charms" / lib.path
-        dest = charm.build_path / "lib" / "charms" / lib.path
-        logger.debug("Copying %s to %s", lib, dest)
-        if not dry_run:
+        for lib in charm.libraries:
+            src = LIBS_CHARM_PATH / "lib" / "charms" / lib.path
+            dest = charm.build_path / "lib" / "charms" / lib.path
+            logger.debug("Copying %s to %s", lib, dest)
+            if not dry_run:
+                copy(src, dest)
+
+        with open(charm.build_path / "pyproject.toml", "r") as fin:
+            pyproject = rtoml.load(fin)
+
+        for pkg in charm.packages:
+            filename = f"{pkg.name.replace('-', '_')}-{pkg.version}.tar.gz"
+            src = BUILD_PATH / "dist" / filename
+            dest = charm.build_path / "dist" / filename
+            logger.debug("Copying %s to %s", src, dest)
             copy(src, dest)
 
-    if not dry_run:
-        UV.run_command(
-            [
-                "--quiet",
-                "export",
-                "--package",
-                charm.name,
-                "--frozen",
-                "--no-hashes",
-                "--no-emit-workspace",
-                "--format=requirements-txt",
-                "-o",
-                str(charm.build_path / "requirements.txt"),
-            ]
-        )
+            # Remove any package names from the dependencies fields that conflicts
+            # with local, vendored packages. Replace removed package name with the file path
+            # of the local equivalent package.
+            try:
+                pyproject["project"]["dependencies"].remove(pkg.name)
+            except ValueError:
+                pass
 
-    if not dry_run:
-        with open(charm.build_path / "requirements.txt", "+a") as f:
-            print("# ===== Local packages =====", file=f)
-            for pkg in charm.packages:
-                filename = f"{pkg.name.replace('-', '_')}-{pkg.version}.tar.gz"
-                src = BUILD_PATH / "dist" / filename
-                dest = charm.build_path / "dist" / filename
-                logger.debug("Copying %s to %s", src, dest)
-                if not dry_run:
-                    copy(src, dest)
-                print(f"./dist/{filename}", file=f)
+            # Inject local subpackage into charm dependencies.
+            pyproject["project"]["dependencies"].append(
+                f"{pkg.name} @ file:///${{PROJECT_ROOT}}/dist/{filename}"
+            )
+
+        with open(charm.build_path / "pyproject.toml", "wt") as fout:
+            rtoml.dump(pyproject, fout)
 
     logger.info("staged charm %s at %s", charm.path.name, charm.build_path)
 
